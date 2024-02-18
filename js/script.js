ab512fe94d651432aaa975bac971b10ad0318129
Md Ataullah <ataullahm100@gmail.com>
2024-02-18 17:14:35 +0600

    Seat add then seat background color added:

--- js/script.js
+++ js/script.js
@@ -3,7 +3,27 @@ console.log('Hello World');
 const seats = document.getElementsByClassName('seat');
 for (const seat of seats) {
   //   console.log(seat);
-  seat.addEventListener('click', function () {
-    console.log('Good Jobs');
+  let seatArr = [];
+
+  seat.addEventListener('click', function (e) {
+    // console.log(e.target);
+    const seat = e.target.innerText;
+    seatArr.push(seat);
+    const setList = document.getElementById('seatReasive');
+    const setListCreate = document.createElement('div');
+    setListCreate.innerHTML = ` <div
+    class="flex justify-between text-base font-normal text-[#03071299]"
+  >
+    <p>${seat}</p>
+    <p>Economoy</p>
+    <p>550</p>
+  </div>`;
+    if (seatArr.includes(seat)) {
+      console.log(seatArr);
+    }
+    setList.appendChild(setListCreate);
+
+    e.target.style.backgroundColor = '#1DD100';
+    e.target.style.color = '#fff';
   });
 }
