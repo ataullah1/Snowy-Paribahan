e34d038417d5c03493a929dcae29e26c768af6e8
Md Ataullah <ataullahm100@gmail.com>
2024-02-18 15:04:11 +0600

    Function adding

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
 console.log('Hello World');
+
+const seats = document.getElementsByClassName('seat');
+for (const seat of seats) {
+  //   console.log(seat);
+  seat.addEventListener('click', function () {
+    console.log('Good Jobs');
+  });
+}
