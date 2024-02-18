4012e4a82d2b2a92bdc349f678c624aa900c0a0f
Md Ataullah <ataullahm100@gmail.com>
2024-02-18 18:52:01 +0600

    seat condition function

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 // console.log('Hello World');
 let seatArr = [];
+let unicArr = [];
 let count = 0;
 
 const seats = document.getElementsByClassName('seat');
@@ -11,26 +12,21 @@ for (const seat of seats) {
     e.target.style.color = '#fff';
     // console.log(e.target);
     const seat = e.target.innerText;
-    const setList = document.getElementById('seatReasive');
+    const seatLists = document.getElementById('seatReasive');
     const countSeat = document.getElementById('countSeat');
     const availableSeat = document.getElementById('seatsNum');
     const seatNum = availableSeat.innerText;
     const btnNext = document.getElementById('btnNext');
 
-    seatArr.push(seat);
-    if (seatArr.includes(seat)) {
-      console.log(seatArr);
-    }
-
     const setListCreate = document.createElement('div');
     setListCreate.innerHTML = ` <div
     class="flex justify-between text-base font-normal text-[#03071299]"
   >
-    <p>${seat}</p>
+    <p class="seatList">${seat}</p>
     <p>Economoy</p>
     <p>550</p>
   </div>`;
-    setList.appendChild(setListCreate);
+    seatLists.appendChild(setListCreate);
     count = count + 1;
     countSeat.innerText = count;
     const availableS = parseInt(seatNum) - 1;
@@ -41,5 +37,20 @@ for (const seat of seats) {
     value="Next"
     class="bg-primary text-white border-2 border-solid border-primary hover:bg-[#fff0] hover:text-primary py-2 sm:py-3 w-full rounded-2xl text-xl font-extrabold cursor-pointer"
   />`;
+    const seatList = document.getElementsByClassName('seatList');
+    let at = [];
+    for (const a of seatList) {
+      const arr = a.innerText;
+      at.push(arr);
+      // console.log(arr);
+    }
+    console.log(at, seat);
+    const b = at.includes(seat);
+    console.log(b);
+    // if (!at.includes(seat)) {
+    //   console.log('dubble');
+    // } else {
+    //   console.log('single');
+    // }
   });
 }
